120aac799a4688e415e936e012a4ad6f1ec1d381
wormzoop <446706489@qq.com>
2018-05-26 13:20:27 +0800

    1

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, Response
 from flask import request
+from user import User,tojson
+import pymysql
+import json
 
 app = Flask(__name__)
 
@@ -9,7 +12,23 @@ def home():
 
 @app.route('/user/list', methods=['GET'])
 def userlist():
-    
+	db = pymysql.connect("rm-2zey1rij96gh594txto.mysql.rds.aliyuncs.com","root","123QWE!@#","heisa")
+	cursor = db.cursor()
+	sql = 'select * from test'
+	list = []
+	try:
+		cursor.execute(sql)
+		results = cursor.fetchall()
+		for row in results:
+			u = User()
+			u.id_ = row[0]
+			u.name = row[1]
+			list.append(u)
+			print(list)
+	except:
+		db.rollback()
+	db.close()
+	return Response(json.dumps(list, default=tojson), 'application/json')
 
 if __name__ == '__main__':
     app.run()
